d308f6b58dfec7b7fd1776eb698d0bc54b68465f
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-01-31 23:49:14 -0500

    Create scenebuilder.h

---
+++ doom_renderer/libs/include/custom/scenebuilder.h
@@ -0,0 +1,34 @@
+#ifndef SCENE_BUILDER_H
+#define SCENE_BUILDER_H
+
+#include <map>
+#include <string>
+#include <glad/glad.h>
+#include <vector>
+#include <custom/doom_map.h>
+#include <custom/doom_data.h>
+
+class SceneBuilder
+{
+    public:
+        SceneBuilder(DoomMap *map_); 
+        ~SceneBuilder();
+        mapsector_t* buildSector(
+            std::vector<mapvertex_t> vertices,
+            std::vector<std::string> ceil_front_texture_names,
+            std::vector<std::string> ceil_back_texture_names,
+            std::vector<std::string> middle_texture_names,
+            std::vector<std::string> floor_front_texture_names,
+            std::vector<std::string> floor_back_texture_names,
+            std::string floor_texture_name,
+            std::string ceil_texture_name,
+            int floor_height,
+            int ceil_height,
+            unsigned int light_level
+            );
+        void render(); //It uses its children to do polygon substraction
+    private:
+        DoomMap *map;
+};
+
+#endif
