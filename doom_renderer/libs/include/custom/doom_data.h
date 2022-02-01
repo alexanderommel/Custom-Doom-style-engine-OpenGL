09c31bad0e60445ba4126fcb84c81c11b8f05ba8
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-01-31 23:48:27 -0500

    Create doom_data.h

---
+++ doom_renderer/libs/include/custom/doom_data.h
@@ -0,0 +1,51 @@
+// Data definitions based on the original Doom Source Code definitions but with some modifications
+// Names are conserved
+
+#ifndef __DOOMDATA__
+#define __DOOMDATA__
+
+#include <string>
+
+struct mapvertex_t
+{
+	int x;
+	int z;
+};
+
+
+struct mapsidedef_t
+{
+	int y_position;
+	unsigned int height;
+	std::string texture_name;
+};
+
+
+struct maplinedef_t
+{
+	struct mapvertex_t v1;
+	struct mapvertex_t v2;
+	struct mapsidedef_t ceil_front_sidedef;
+	struct mapsidedef_t ceil_back_sidedef;
+	struct mapsidedef_t middle_sidedef;
+	struct mapsidedef_t floor_front_sidedef;
+	struct mapsidedef_t floor_back_sidedef;
+};
+
+struct map_flatplane_t
+{
+	int y_position;
+	std::string texture_name;
+};
+
+struct mapsector_t
+{
+	struct mapvertex_t* vertices;
+	struct maplinedef_t* linedefs;
+	struct map_flatplane_t floor_plane;
+	struct map_flatplane_t ceil_plane;
+	unsigned int ilumination;
+	bool compiled;
+};
+
+#endif
