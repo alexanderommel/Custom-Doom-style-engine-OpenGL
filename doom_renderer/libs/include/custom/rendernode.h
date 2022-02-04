a28d3a02d52ec208abaf23e14e9bd18649088639
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-02-03 23:56:30 -0500

    Updated rendernode header (it must use a line and plane renderer)

--- doom_renderer/libs/include/custom/rendernode.h
+++ doom_renderer/libs/include/custom/rendernode.h
@@ -1,31 +1,39 @@
 #ifndef RENDER_NODE_H
 #define RENDER_NODE_H
 
-#include <map>
 #include <string>
 #include <glad/glad.h>
 #include <vector>
 #include <custom/doom_data.h>
-
-
-
-
+#include <custom/shader.h>
+#include <custom/line_renderer.h>
+#include <custom/plane_renderer.h>
 
 class RenderNode
-
-
 {
     public:
-        RenderNode(unsigned int id); // There's not default data
+        RenderNode(unsigned int id, Shader *line_shader_, Shader *plane_shader_); // There's not default data
         ~RenderNode();
-        void addChild(RenderNode *node);
-        void addSector(mapsector_t *sector_);
+        void addChild(RenderNode* node);
+        void addSector(mapsector_t* sector_);
         bool hasChildren();
         void render(); //It uses its children to do polygon substraction
         void printContent();
         unsigned int id; 
         mapsector_t *sector;
+        Shader* line_shader;
+        Shader* plane_shader;
+        LineRenderer* line_renderer;
+        PlaneRenderer* plane_renderer;
         std::vector<RenderNode*> children;
+    private:
+        void renderMiddleSidedef();
+        void renderCeilFrontSidedef();
+        void renderCeilBackSidedef();
+        void renderFloorFrontSidedef();
+        void renderFloorBackSidedef();
+        void renderCeilPlane();
+        void renderFloorPlane();
 };
 
 #endif
