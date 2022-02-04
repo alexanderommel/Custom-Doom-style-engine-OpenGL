202dbe646df775c527bcbdf0f4b7253839e3537e
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-02-04 00:04:51 -0500

    Scene rendering uses post-order tree traversal
    Doom Renderer uses stencil test for clipping sub sector planes from parent sectors.

--- doom_renderer/libs/include/custom/rendertree.h
+++ doom_renderer/libs/include/custom/rendertree.h
@@ -6,24 +6,25 @@
 #include <glad/glad.h>
 #include <vector>
 #include <custom/rendernode.h>
-
-
-
+#include <custom/doom_data.h>
+#include <custom/shader.h>
 
 
 class RenderTree
 
-
-
 {
     public:
-        RenderTree(); 
+        RenderTree(Shader *line_shader_, Shader *plane_shader_); 
         ~RenderTree();
         void setRootNode(RenderNode *root);
         void trasversalOrderPrintContent();
         bool isRootNodeNull();
         void render(); //It uses its children to do polygon substraction
         RenderNode *root_node;
+        Shader* line_shader;
+        Shader* plane_shader;
+    private:
+        void postOrderRendering(RenderNode* node);
 };
 
 #endif
