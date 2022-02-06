6278d950ece16397d069282ecd2e9a7442c741e8
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-02-05 23:07:15 -0500

    Update render_tree.cpp

--- doom_renderer/doom_renderer/render_tree.cpp
+++ doom_renderer/doom_renderer/render_tree.cpp
@@ -1,9 +1,12 @@
 #include <custom/rendertree.h>
 #include <custom/rendernode.h>
 #include <iostream>
+#include <custom/shader.h>
 
-RenderTree::RenderTree(){
+RenderTree::RenderTree(Shader *line_shader_, Shader *plane_shader_){
 	this->root_node = NULL;
+	this->line_shader = line_shader_;
+	this->plane_shader = plane_shader_;
 }
 
 RenderTree::~RenderTree(){
@@ -18,11 +21,41 @@ bool RenderTree::isRootNodeNull() {
 }
 
 void RenderTree::render() {
-	std::cout << "Rendering Tree..\n";
+	std::cout << "(RenderTree) Rendering Tree..\n";
+	//this->root_node->render();
+	postOrderRendering(this->root_node);
+}
+
+void RenderTree::postOrderRendering(RenderNode* node) {
+	if (node->hasChildren() == false)
+	{
+		node->render();
+		return;
+	}
+	std::vector<map_flatplane_t> ceil_clipping_planes;
+	std::vector<map_flatplane_t> floor_clipping_planes;
+	std::vector<mapvertex_t*> clipping_vertices;
+	for (size_t i = 0; i < node->children.size(); i++)
+	{
+		RenderNode* child = node->children[i];
+		// Load clipping data for Stencil
+		map_flatplane_t ceil_clipplin_plane = child->sector->ceil_plane;
+		map_flatplane_t floor_clipping_plane = child->sector->floor_plane;
+		mapvertex_t* node_vertices = child->sector->vertices;
+		// Saving clipping data
+		ceil_clipping_planes.push_back(ceil_clipplin_plane);
+		floor_clipping_planes.push_back(floor_clipping_plane);
+		clipping_vertices.push_back(node_vertices);
+		// Rendering child
+		postOrderRendering(child);
+	}
+	// Clip sectors using Stencil Test
+	// Render current node 'parent'
+	node->render();
 }
 
 void RenderTree::trasversalOrderPrintContent() {
-	std::cout << "Printing content of the tree..\n";
+	std::cout << "(RenderTree) Printing content of the tree..\n";
 	if (isRootNodeNull())
 	{
 		std::cout << "No root node linked\n";
