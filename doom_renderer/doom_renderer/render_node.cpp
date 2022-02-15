76b63c599efefb1900771b1681c53a0b112f6ac9
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-02-15 17:55:13 -0500

    Update render_node.cpp

--- doom_renderer/doom_renderer/render_node.cpp
+++ doom_renderer/doom_renderer/render_node.cpp
@@ -41,7 +41,7 @@ void RenderNode::renderMiddleSidedef() {
 			this->sector->linedefs[i].v2,
 			this->sector->linedefs[i].middle_sidedef.height,
 			this->sector->linedefs[i].middle_sidedef.y_position,
-			glm::vec3(1.0f, 1.0f, 1.0f));
+			glm::vec3(1.0f, 1.0f, 1.0f),this->sector->ilumination);
 		glDisable(GL_CULL_FACE);
 	}
 }
@@ -50,6 +50,14 @@ void RenderNode::renderCeilFrontSidedef() {
 	//std::cout << "(RenderNode) Rendering ceil front sidedefs\n";
 	for (size_t i = 0; i < this->sector->num_vertices; i++)
 	{
+		if (this->sector->linedefs[i].is_shared)
+		{
+			if (this->sector->linedefs[i].neighbor_sector->ceil_plane.y_position ==
+				this->sector->ceil_plane.y_position)
+			{
+				continue;
+			}
+		}
 		//std::cout << "(RenderNode) Rendering sidedef " << i << "\n";
 		this->line_shader->Use();
 		Texture2D tex1 = ResourceManager::GetTexture(this->sector->linedefs[i].ceil_front_sidedef.texture_name);
@@ -59,7 +67,7 @@ void RenderNode::renderCeilFrontSidedef() {
 			this->sector->linedefs[i].v2,
 			this->sector->linedefs[i].ceil_front_sidedef.height,
 			this->sector->linedefs[i].ceil_front_sidedef.y_position,
-			glm::vec3(1.0f, 1.0f, 1.0f));
+			glm::vec3(1.0f, 1.0f, 1.0f), this->sector->parent_ilumination);
 	}
 }
 
@@ -67,6 +75,14 @@ void RenderNode::renderCeilBackSidedef() {
 	//std::cout << "(RenderNode) Rendering ceil back sidedefs\n";
 	for (size_t i = 0; i < this->sector->num_vertices; i++)
 	{
+		if (this->sector->linedefs[i].is_shared)
+		{
+			if (this->sector->linedefs[i].neighbor_sector->ceil_plane.y_position ==
+				this->sector->ceil_plane.y_position)
+			{
+				continue;
+			}
+		}
 		//std::cout << "(RenderNode) Rendering sidedef " << i << "\n";
 		this->line_shader->Use();
 		Texture2D tex1 = ResourceManager::GetTexture(this->sector->linedefs[i].ceil_back_sidedef.texture_name);
@@ -76,7 +92,7 @@ void RenderNode::renderCeilBackSidedef() {
 			this->sector->linedefs[i].v2,
 			this->sector->linedefs[i].ceil_back_sidedef.height,
 			this->sector->linedefs[i].ceil_back_sidedef.y_position,
-			glm::vec3(1.0f, 1.0f, 1.0f));
+			glm::vec3(1.0f, 1.0f, 1.0f), this->sector->ilumination);
 	}
 }
 
@@ -84,7 +100,27 @@ void RenderNode::renderFloorFrontSidedef() {
 	//std::cout << "(RenderNode) Rendering floor front sidedefs\n";
 	for (size_t i = 0; i < this->sector->num_vertices; i++)
 	{
-		//std::cout << "(RenderNode) Rendering sidedef " << i << "\n";
+		if (this->sector->linedefs[i].is_shared)
+		{
+			if (this->sector->linedefs[i].neighbor_sector->floor_plane.y_position >=
+				this->sector->floor_plane.y_position)
+			{
+				continue;
+			}
+		}
+		//Configure ilumination values
+		//If this sidedef is a neighbor sidedef then it uses neighbor sector ilumination
+		//the otherway it uses parent ilumination
+		float sidedef_ilumination_level = 1.0f;
+		if (this->sector->linedefs[i].is_shared)
+		{
+			//std::cout << " (RenderNode) Shared linedef found!!!!\n";
+			sidedef_ilumination_level = this->sector->linedefs[i].neighbor_sector->ilumination;
+		}
+		else {
+			sidedef_ilumination_level = this->sector->parent_ilumination;
+			//sidedef_ilumination_level = 0.0f;
+		}
 		this->line_shader->Use();
 		Texture2D tex1 = ResourceManager::GetTexture(this->sector->linedefs[i].floor_front_sidedef.texture_name);
 		tex1.Bind();
@@ -93,7 +129,7 @@ void RenderNode::renderFloorFrontSidedef() {
 			this->sector->linedefs[i].v2,
 			this->sector->linedefs[i].floor_front_sidedef.height,
 			this->sector->linedefs[i].floor_front_sidedef.y_position,
-			glm::vec3(1.0f, 1.0f, 1.0f));
+			glm::vec3(1.0f, 1.0f, 1.0f), sidedef_ilumination_level);
 	}
 }
 
@@ -101,6 +137,14 @@ void RenderNode::renderFloorBackSidedef() {
 	//std::cout << "(RenderNode) Rendering floor back sidedefs\n";
 	for (size_t i = 0; i < this->sector->num_vertices; i++)
 	{
+		if (this->sector->linedefs[i].is_shared)
+		{
+			if (this->sector->linedefs[i].neighbor_sector->floor_plane.y_position==
+				this->sector->floor_plane.y_position)
+			{
+				continue;
+			}
+		}
 		//std::cout << "(RenderNode) Rendering sidedef " << i << "\n";
 		this->line_shader->Use();
 		Texture2D tex1 = ResourceManager::GetTexture(this->sector->linedefs[i].floor_back_sidedef.texture_name);
@@ -110,7 +154,7 @@ void RenderNode::renderFloorBackSidedef() {
 			this->sector->linedefs[i].v2,
 			this->sector->linedefs[i].floor_back_sidedef.height,
 			this->sector->linedefs[i].floor_back_sidedef.y_position,
-			glm::vec3(1.0f, 1.0f, 1.0f));
+			glm::vec3(1.0f, 1.0f, 1.0f), this->sector->ilumination);
 	}
 }
 
@@ -126,7 +170,8 @@ void RenderNode::renderCeilPlane() {
 		this->sector->num_vertices,
 		this->sector->ceil_plane.y_position,
 		6,
-		glm::vec3(1.0f, 1.0f, 1.0f), this->sector->flatplane_vertex_array,sector_width);
+		glm::vec3(1.0f, 1.0f, 1.0f), 
+		this->sector->flatplane_vertex_array,sector_width,this->sector->ilumination);
 }
 
 void RenderNode::renderFloorPlane() {
@@ -142,13 +187,22 @@ void RenderNode::renderFloorPlane() {
 		this->sector->num_vertices,
 		this->sector->floor_plane.y_position,
 		6,
-		glm::vec3(1.0f, 1.0f, 1.0f),this->sector->flatplane_vertex_array, sector_width);
+		glm::vec3(1.0f, 1.0f, 1.0f),this->sector->flatplane_vertex_array, sector_width,
+		this->sector->ilumination);
 }
 
 void RenderNode::render() {
 
 	//std::cout << "(RenderNode) Rendering node with id->" << this->id << "\n";
 
+	// Render flat planes
+	//glEnable(GL_DEPTH_TEST);
+	renderCeilPlane();
+	renderFloorPlane();
+
+	glDisable(GL_STENCIL_TEST);
+	glStencilMask(0xFF);
+	glStencilFunc(GL_ALWAYS, 1, 0xFF);
 	
 	// Render linedefs
 	if (this->id==0)
@@ -175,10 +229,7 @@ void RenderNode::render() {
 		}
 	}
 	
-	// Render flat planes
-
-	renderCeilPlane();
-	renderFloorPlane();
+	
 	
 }
 
