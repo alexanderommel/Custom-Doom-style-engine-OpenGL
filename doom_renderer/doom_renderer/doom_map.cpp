38ed30d4bcfe5bb4669d8401c177baf786a7a311
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-02-20 23:20:27 -0500

    Update doom_map.cpp

--- doom_renderer/doom_renderer/doom_map.cpp
+++ doom_renderer/doom_renderer/doom_map.cpp
@@ -23,10 +23,10 @@ void DoomMap::init() {
 // Updates the values of the mapsector using the values of the parent sector
 void DoomMap::compileSector(mapsector_t* sector, mapsector_t* parent) {
 
-	std::cout << "(DoomMap) compiling sector...\n";
+	//std::cout << "(DoomMap) compiling sector...\n";
 	if (sector->linedefs[0].has_ms==true)
 	{
-		std::cout << "(DoomMap) updating main sector sidedefs...\n";
+		//std::cout << "(DoomMap) updating main sector sidedefs...\n";
 		// Sidedef y position is equal to the floor plane position
 		// Sidedef height is equal to the absolute value of floor_position - ceil_position
 		unsigned int height = abs(sector->ceil_plane.y_position - sector->floor_plane.y_position);
@@ -39,7 +39,7 @@ void DoomMap::compileSector(mapsector_t* sector, mapsector_t* parent) {
 	}
 	if (parent!=NULL)
 	{
-		std::cout << "(DoomMap) updating subsector sidedefs...\n";
+		//std::cout << "(DoomMap) updating subsector sidedefs...\n";
 		unsigned int ceil_height = abs(sector->ceil_plane.y_position - parent->ceil_plane.y_position);
 		unsigned int floor_height = abs(sector->floor_plane.y_position - parent->floor_plane.y_position);
 		if (sector->linedefs[0].has_cfs == true)
@@ -89,20 +89,64 @@ void DoomMap::compileSector(mapsector_t* sector, mapsector_t* parent) {
 		// linedef.neighbor_sector=&sector
 	}
 
-
-	std::cout << "(DoomMap) updating linedef vertices...\n";
+	//std::cout << "(DoomMap) #################################################################>\n";
+	//std::cout << "(DoomMap) Updating Sector Linedef Vertices--------------------------->\n";
+	//std::cout << "(DoomMap) updating linedef vertices...\n";
+	glm::vec3 y_axis = glm::vec3(0, 1, 0);
 	for (unsigned int j = 0; j < sector->num_vertices-1; j++)
 	{
 		 mapvertex_t v1 = sector->vertices[j];
 		 mapvertex_t v2 = sector->vertices[j + 1];
 		sector->linedefs[j].v1 = v1;
 		sector->linedefs[j].v2 = v2;
+
+		//std::cout << "(DoomMap) Vertices order v1=<" << v1.x << ",0," << v1.z << ">--->v2=<" << v2.x << ",0," << v2.z << ">\n";
+
+
+		// Compute normal vectors for the linedef
+		// 1. Director vector
+		glm::vec3 director = glm::vec3(v2.x, 0, v2.z) - glm::vec3(v1.x, 0, v1.z);
+		glm::vec3 normal_ = glm::cross(y_axis, director);
+		normal_ = glm::normalize(normal_);
+		sector->linedefs[j].normal = normal_;
+
+		//std::cout << "(DoomMap) Normal vector is n=<" << normal_.x << ",0," << normal_.z << ">\n";
 	}
 	sector->linedefs[sector->num_vertices - 1].v1 = sector->vertices[sector->num_vertices - 1];
 	sector->linedefs[sector->num_vertices - 1].v2 = sector->vertices[0];
 
+	// compute normal vectors for last segment
+	mapvertex_t v11 = sector->vertices[sector->num_vertices - 1];
+	mapvertex_t v22 = sector->vertices[0];
+
+	//std::cout << "(DoomMap) Vertices order v1=<" << v11.x << "," << v11.z << ">--->v2=<" << v22.x << "," << v22.z << ">\n";
+
+	glm::vec3 director2 = glm::vec3(v22.x, 0, v22.z) - glm::vec3(v11.x, 0, v11.z);
+	glm::vec3 normal2_ = glm::cross(y_axis, director2);
+	normal2_ = glm::normalize(normal2_);
+	sector->linedefs[sector->num_vertices - 1].normal = normal2_;
+
+	//std::cout << "(DoomMap) Normal vector is NN=<" << normal2_.x << ",0," << normal2_.z << ">\n";
+
+
+	// determine if sector defined counter or clockwise
+
+
+	bool counterclock1 = false;
+	glm::vec3 cross_vector = glm::cross(glm::vec3(v11.x, 0, v11.z), glm::vec3(v22.x, 0, v22.z));
+	if (cross_vector.y>0)
+	{
+		counterclock1 = true;
+	}
+
+	sector->counterclock = counterclock1;
+
+	//std::cout << "(DoomMap) Sector is counterclock->" << counterclock1 << "\n";
+
+	//std::cout << "(DoomMap)#################################\n";
+
 	// Create vertex arrays for rendering planes
-	std::cout << "(DoomMap) updating flat_planes vertex array\n";
+	//std::cout << "(DoomMap) updating flat_planes vertex array\n";
 
 	// each vertex has 4 coordinates
 
@@ -139,19 +183,19 @@ void DoomMap::compileSector(mapsector_t* sector, mapsector_t* parent) {
 	}
 	sector->flatplane_vertex_array = points;
 
-	std::cout << "(DoomMap) compilation succesful!\n";
+	//std::cout << "(DoomMap) compilation succesful!\n";
 }
 
 // Add the mapsector to the respective node in the tree
 bool DoomMap::addSectorToRenderTree(mapsector_t* sector) {
-	std::cout << "(DoomMap) Adding sector to the render tree...\n";
+	//std::cout << "(DoomMap) Adding sector to the render tree...\n";
 	// Test if the tree has a root sector
 	if (this->render_tree->isRootNodeNull())
 	{
 		if (sector->linedefs[0].has_ms==true)
 		{
 			compileSector(sector,NULL);
-			std::cout << "(DoomMap) Setting root RenderNode\n";
+			//std::cout << "(DoomMap) Setting root RenderNode\n";
 			static RenderNode *root_node = new RenderNode(0, this->line_shader, this->plane_shader);
 			root_node->sector = sector;
 			this->render_tree->setRootNode(root_node);
@@ -164,16 +208,16 @@ bool DoomMap::addSectorToRenderTree(mapsector_t* sector) {
 
 	int depth = 0;
 	int breadth = 0;
-	std::cout << "(DoomMap) Breadth first search for parent sectors\n";
+	//std::cout << "(DoomMap) Breadth first search for parent sectors\n";
 	std::queue<RenderNode*> queue; 
 	queue.push(this->render_tree->root_node);
 	while (queue.empty()==false)
 	{
 		depth = depth + 1;
 		RenderNode* current = queue.back();
-		std::cout << "(DoomMap) Current node id is: " << current->id << "\n";
+		//std::cout << "(DoomMap) Current node id is: " << current->id << "\n";
 		queue.pop();
-		std::cout << "(DoomMap) Queue size is: " << queue.size() << "\n";
+		//std::cout << "(DoomMap) Queue size is: " << queue.size() << "\n";
 		std::vector<RenderNode*> children = current->children;
 		// It is assured that there's at most one child node that has a parent sector
 		// We assume that the current sector is child of root sector!!!
@@ -181,11 +225,11 @@ bool DoomMap::addSectorToRenderTree(mapsector_t* sector) {
 		for (size_t i = 0; i < children.size(); i++)
 		{
 			RenderNode* child = children[i];
-			std::cout << "(DoomMap) Testing child with id: " << child->id << "\n";
+			//std::cout << "(DoomMap) Testing child with id: " << child->id << "\n";
 			bool subsector_test = isSubsector(child->sector, sector);
 			if (subsector_test)
 			{
-				std::cout << "(DoomMap) Testing passed...parent node found\n";
+				//std::cout << "(DoomMap) Testing passed...parent node found\n";
 				queue.push(child);
 				breadth = i;
 				child_parent_found = true;
@@ -196,8 +240,8 @@ bool DoomMap::addSectorToRenderTree(mapsector_t* sector) {
 		if (child_parent_found==false)
 		{
 			//std::cout << "(DoomMap) No child is parent of current sector\n";
-			std::cout << "(DoomMap) Linking this sector to the current parent sector\n";
-			std::cout << "(DoomMap) Current parent id is: " << current->id << "\n";
+			//std::cout << "(DoomMap) Linking this sector to the current parent sector\n";
+			//std::cout << "(DoomMap) Current parent id is: " << current->id << "\n";
 			// Compile basic info
 			compileSector(sector, current->sector);
 			// Compile info related to lights
@@ -205,9 +249,9 @@ bool DoomMap::addSectorToRenderTree(mapsector_t* sector) {
 
 			int node_id = depth*1000;
 			node_id = node_id + breadth;
-			std::cout << "(DoomMap) Generating new id with id=" << node_id << "\n";
+			//std::cout << "(DoomMap) Generating new id with id=" << node_id << "\n";
 			RenderNode* new_node = new RenderNode(node_id, this->line_shader, this->plane_shader);
-			std::cout << "(DoomMap) Current child id is: " << new_node->id << "\n";
+			//std::cout << "(DoomMap) Current child id is: " << new_node->id << "\n";
 			new_node->sector = sector;
 			new_node->children = std::vector<RenderNode*>();
 			current->children.push_back(new_node);
@@ -218,7 +262,7 @@ bool DoomMap::addSectorToRenderTree(mapsector_t* sector) {
 }
 
 bool DoomMap::isSubsector(mapsector_t* parent, mapsector_t* sector) {
-	std::cout << "(DoomMap) Testing subsector condition\n";
+	//std::cout << "(DoomMap) Testing subsector condition\n";
 	// All values from pnpoly must be equal to 1 in order for this to become a subsector
 	// A subsector has a subsector_test equal to num_vertices
 	// A neighbor sector has a subsector_test equal to 0
@@ -255,7 +299,7 @@ void DoomMap::update() {
 
 }
 void DoomMap::render() {
-	std::cout << "(DoomMap) Map rendering\n";
+	//std::cout << "(DoomMap) Map rendering\n";
 	this->render_tree->render();
 	//this->render_tree->trasversalOrderPrintContent();
 }
@@ -276,6 +320,7 @@ void setNeighborSectors(mapsector_t* s1, std::vector<RenderNode*> neighbors) {
 			if (found_neighbor)
 			{
 				//std::cout << "(DoomMap) Neighbor found for linedef!\n";
+				//std::cout << "(DoomMap) Neighbor id is -> " << neighbors[j] << "\n";
 				break;
 			}
 		}
@@ -287,18 +332,84 @@ bool isLinedefSharedWithSector(maplinedef_t* linedef, mapsector_t* sector, mapse
 	//std::cout << "(DoomMap) Testing shared status\n";
 	for (size_t i = 0; i < neighbor_sector_->num_vertices; i++)
 	{
-		maplinedef_t neighbor_linedef = neighbor_sector_->linedefs[i];
-		if ((linedef->v1.x==neighbor_linedef.v2.x) &&
-			(linedef->v2.x == neighbor_linedef.v1.x) &&
-			(linedef->v1.z == neighbor_linedef.v2.z) &&
-			(linedef->v2.z == neighbor_linedef.v1.z))
+		maplinedef_t* neighbor_linedef = &neighbor_sector_->linedefs[i];
+		if ((linedef->v1.x==neighbor_linedef->v2.x) &&
+			(linedef->v2.x == neighbor_linedef->v1.x) &&
+			(linedef->v1.z == neighbor_linedef->v2.z) &&
+			(linedef->v2.z == neighbor_linedef->v1.z))
 		{
 			linedef->is_shared = true;
+			neighbor_linedef->is_shared = true;
+			neighbor_linedef->neighbor_sector = sector;
 			linedef->neighbor_sector = neighbor_sector_;
-			neighbor_linedef.is_shared = true;
-			neighbor_linedef.neighbor_sector = sector;
+			if (sector->floor_plane.y_position>=neighbor_sector_->floor_plane.y_position)
+			{
+				linedef->floor_front_sidedef.height = sector->floor_plane.y_position - neighbor_sector_->floor_plane.y_position;
+				linedef->floor_front_sidedef.y_position = neighbor_sector_->floor_plane.y_position;
+			}
+			if (neighbor_sector_->floor_plane.y_position <= sector->floor_plane.y_position)
+			{
+
+			}
+			
+			
 			return true;
 		}
 	}
 	return false;
+}
+
+void DoomMap::addDirectionalLight(DirLight light) {
+	this->line_shader->Use();
+	this->line_shader->SetVector3f("dirLight.direction", light.direction);
+	this->line_shader->SetVector3f("dirLight.ambient", light.ambient);
+	this->line_shader->SetVector3f("dirLight.diffuse", light.diffuse);
+	this->line_shader->SetVector3f("dirLight.specular", light.specular);
+	this->line_shader->SetVector3f("material.specular", 0.3, 0.3, 0.3);
+	
+	this->plane_shader->Use();
+	this->plane_shader->SetVector3f("dirLight.direction", light.direction);
+	this->plane_shader->SetVector3f("dirLight.ambient", light.ambient);
+	this->plane_shader->SetVector3f("dirLight.diffuse", light.diffuse);
+	this->plane_shader->SetVector3f("dirLight.specular", light.specular);
+	this->plane_shader->SetVector3f("material.specular", 0.3, 0.3, 0.3);
+}
+void DoomMap::addPointLight(PointLight light, int light_pos) {
+	std::string pointLights = "pointLights";
+	std::string left_bracket = "[";
+	std::string right_brackets = "]";
+	std::string index = left_bracket + std::to_string(light_pos);
+	index = index + right_brackets;
+	std::string shaderAttribute = pointLights + index;
+	shaderAttribute = shaderAttribute + ".";
+	std::string position_str = shaderAttribute + "position";
+	std::string ambient_str = shaderAttribute + "ambient";
+	std::string diffuse_str = shaderAttribute + "diffuse";
+	std::string specular_str = shaderAttribute + "specular";
+	std::string constant_str = shaderAttribute + "constant";
+	std::string linear_str = shaderAttribute + "linear";
+	std::string quadratic_str = shaderAttribute + "quadratic";
+	std::cout << "(DoomMap) Shadder att: " << quadratic_str << "\n";
+	
+	// point light 
+	this->line_shader->Use();
+	this->line_shader->SetInteger("num_lights", light_pos);
+	this->line_shader->SetVector3f(position_str.c_str(), light.position);
+	this->line_shader->SetVector3f(ambient_str.c_str(), light.ambient);
+	this->line_shader->SetVector3f(diffuse_str.c_str(), light.diffuse);
+	this->line_shader->SetVector3f(specular_str.c_str(), light.specular);
+	this->line_shader->SetFloat(constant_str.c_str(), light.constant);
+	this->line_shader->SetFloat(linear_str.c_str(), light.linear);
+	this->line_shader->SetFloat(quadratic_str.c_str(), light.quadratic);
+
+	this->plane_shader->Use();
+	this->plane_shader->SetInteger("num_lights", light_pos);
+	this->plane_shader->SetVector3f(position_str.c_str(), light.position);
+	this->plane_shader->SetVector3f(ambient_str.c_str(), light.ambient);
+	this->plane_shader->SetVector3f(diffuse_str.c_str(), light.diffuse);
+	this->plane_shader->SetVector3f(specular_str.c_str(), light.specular);
+	this->plane_shader->SetFloat(constant_str.c_str(), light.constant);
+	this->plane_shader->SetFloat(linear_str.c_str(), light.linear);
+	this->plane_shader->SetFloat(quadratic_str.c_str(), light.quadratic);
+	
 }
