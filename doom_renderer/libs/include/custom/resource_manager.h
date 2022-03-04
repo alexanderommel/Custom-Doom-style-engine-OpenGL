3657132b28156f162cdc677f3fe64f232e41d54d
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-03-04 09:47:32 -0500

    Update resource_manager.h

--- doom_renderer/libs/include/custom/resource_manager.h
+++ doom_renderer/libs/include/custom/resource_manager.h
@@ -13,31 +13,24 @@
 #include <string>
 
 #include <glad/glad.h>
-
+//#include <custom/model.h>
 #include <custom/texture.h>
 #include <custom/shader.h>
 
 
-// A static singleton ResourceManager class that hosts several
-// functions to load Textures and Shaders. Each loaded texture
-// and/or shader is also stored for future reference by string
-// handles. All functions and resources are static and no 
-// public constructor is defined.
 class ResourceManager
 {
 public:
     // resource storage
     static std::map<std::string, Shader>    Shaders;
     static std::map<std::string, Texture2D> Textures;
-    // loads (and generates) a shader program from file loading vertex, fragment (and geometry) shader's source code. If gShaderFile is not nullptr, it also loads a geometry shader
+    //static std::map<std::string, Model> Models;
     static Shader    LoadShader(const char *vShaderFile, const char *fShaderFile, const char *gShaderFile, std::string name);
-    // retrieves a stored sader
     static Shader    GetShader(std::string name);
-    // loads (and generates) a texture from file
-    static Texture2D LoadTexture(const char *file, bool alpha, std::string name);
-    // retrieves a stored texture
+    static Texture2D LoadTexture(const char *file, bool alpha, std::string name, float shininess, bool noSpecularColor);
     static Texture2D GetTexture(std::string name);
-    // properly de-allocates all loaded resources
+    //static Model Load3DModel(std::string path,std::string name);
+    //static Model GetModel(std::string name);
     static void      Clear();
 private:
     // private constructor, that is we do not want any actual resource manager objects. Its members and functions should be publicly available (static).
@@ -45,7 +38,8 @@ private:
     // loads and generates a shader from file
     static Shader    loadShaderFromFile(const char *vShaderFile, const char *fShaderFile, const char *gShaderFile = nullptr);
     // loads a single texture from file
-    static Texture2D loadTextureFromFile(const char *file, bool alpha);
+    static Texture2D loadTextureFromFile(const char *file, bool alpha, float shininess, bool noSpecularColor);
+    //static Model loadModelFromFile(std::string path);
 };
 
 #endif
