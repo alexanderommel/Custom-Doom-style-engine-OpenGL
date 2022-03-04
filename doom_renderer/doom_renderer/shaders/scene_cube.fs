0946d4fe7cf14ed1a02470db74883d32ded77733
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-03-04 09:45:22 -0500

    cube shaders

---
+++ doom_renderer/doom_renderer/shaders/scene_cube.fs
@@ -0,0 +1,9 @@
+#version 330 core
+out vec4 FragColor;
+in vec2 TexCoord;
+uniform sampler2D texture1;
+
+void main(){
+	FragColor = texture(texture1, TexCoord);
+	//FragColor = vec4(1.0,0.0,0.0,1.0);
+}
