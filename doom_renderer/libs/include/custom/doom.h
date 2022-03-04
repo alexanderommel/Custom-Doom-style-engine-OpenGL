866d0ef21ce95ace2265f58eceb5ea04c5a8dcf2
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-03-04 09:46:36 -0500

    Update doom.h

--- doom_renderer/libs/include/custom/doom.h
+++ doom_renderer/libs/include/custom/doom.h
@@ -1,28 +1,29 @@
-#ifndef DOOM_H
-#define DOOM_H
+#ifndef _DOOM_H
+#define _DOOM_H
 
-#include <map>
+//#include <map>
 #include <string>
 #include <glad/glad.h>
-#include <vector>
 #include <custom/doom_map.h>
-#include <custom/doom_data.h>
+//#include <custom/doom_data.h>
+//#include <custom/model.h>
 
 class Doom
 {
     public:
         Doom(unsigned int width_, unsigned int height_, unsigned int depth_); 
         ~Doom();
-        unsigned int width;
-        unsigned int height;
-        unsigned int depth;
+        unsigned int widthE;
+        unsigned int heightE;
+        unsigned int depthE;
         DoomMap* map;
         void init();
-        void update(float dt);
+        void update(float time);
         void render(); 
-        void updateProjectionAndView(glm::mat4 view, glm::mat4 proj);
+        void updateProjectionAndView(glm::mat4 view, glm::mat4 proj, glm::vec3 cameraPos);
     private:
-        void loadTextures();
+        void AloadTextures3();
+        //void loadModels();
 };
 
 #endif
